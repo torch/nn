81a1afc194e2ec283272a2652c3a2be4f99b872d
Clement Farabet <clement.farabet@gmail.com>
2012-03-12 17:18:58 -0400

    Fixed Jacobian test. Missing zero() in IO tester.

--- Jacobian.lua
+++ Jacobian.lua
@@ -154,6 +154,7 @@ function nn.Jacobian.testIO(module,input, minval, maxval)
    -- run module
    module:forward(input)
    local go = module.output:clone():copy(torch.rand(module.output:nElement()):mul(inrange):add(minval))
+   module:zeroGradParameters()
    module:updateGradInput(input,go)
    module:accGradParameters(input,go)
 
@@ -167,6 +168,7 @@ function nn.Jacobian.testIO(module,input, minval, maxval)
    -- read module
    local m = torch.DiskFile('tmp.bin'):binary():readObject()
    m:forward(input)
+   module:zeroGradParameters()
    m:updateGradInput(input,go)
    m:accGradParameters(input,go)
    -- cleanup
