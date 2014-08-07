08686ba93022b25eca208c273a2fdf3f084c3adf
Wojciech Zaremba <wojciechz@google.com>
2014-08-07 16:33:39 -0700

    Allows to use Criterion seamlessly in nngraph.

--- ClassNLLCriterion.lua
+++ ClassNLLCriterion.lua
@@ -6,39 +6,47 @@ function ClassNLLCriterion:__init()
 end
 
 function ClassNLLCriterion:updateOutput(input, target)
-   if input:dim() == 1 then
-      self.output = -input[target]
-   elseif input:dim() == 2 then
-      local output = 0
-      for i=1,target:size(1) do
-         output = output - input[i][target[i]]
-      end
-      if self.sizeAverage then
-         output = output / target:size(1)
-      end
-      self.output = output
-   else
-      error('matrix or vector expected')
-   end
-   return self.output
+  if type(input) == 'table' then
+    target = input[2]
+    input = input[1]
+  end
+  if input:dim() == 1 then
+    self.output = -input[target]
+  elseif input:dim() == 2 then
+    local output = 0
+    for i=1,target:size(1) do
+      output = output - input[i][target[i]]
+    end
+    if self.sizeAverage then
+      output = output / target:size(1)
+    end
+    self.output = output
+  else
+    error('matrix or vector expected')
+  end
+  return self.output
 end
 
 function ClassNLLCriterion:updateGradInput(input, target)
-   self.gradInput:resizeAs(input)
-   self.gradInput:zero()
+  if type(input) == 'table' then
+    target = input[2]
+    input = input[1]
+  end
+  self.gradInput:resizeAs(input)
+  self.gradInput:zero()
 
   if input:dim() == 1 then
-      self.gradInput[target] = -1
+    self.gradInput[target] = -1
   else
-      local z = -1
-      if self.sizeAverage then
-         z = z / target:size(1)
-      end
-      local gradInput = self.gradInput
-      for i=1,target:size(1) do
-         gradInput[i][target[i]] = z
-      end
-   end
+    local z = -1
+    if self.sizeAverage then
+      z = z / target:size(1)
+    end
+    local gradInput = self.gradInput
+    for i=1,target:size(1) do
+      gradInput[i][target[i]] = z
+    end
+  end
 
-   return self.gradInput
+  return self.gradInput
 end
