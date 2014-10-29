e155753e8e09f55077fd882cd0e2d15c7f90f428
Nicholas Leonard <nick@nikopia.org>
2014-10-29 10:44:44 -0400

    refactored nn.Linear into C/CUDA methods

--- Linear.lua
+++ Linear.lua
@@ -8,6 +8,7 @@ function Linear:__init(inputSize, outputSize)
    self.gradWeight = torch.Tensor(outputSize, inputSize)
    self.gradBias = torch.Tensor(outputSize)
    
+   self:setup()
    self:reset()
 end
 
@@ -30,69 +31,29 @@ function Linear:reset(stdv)
    end
 end
 
-function Linear:updateOutput(input)
-   if input:dim() == 1 then
-      self.output:resize(self.bias:size(1))
-      self.output:copy(self.bias)
-      self.output:addmv(1, self.weight, input)
-   elseif input:dim() == 2 then
-      local nframe = input:size(1)
-      local nunit = self.bias:size(1)
-
-      self.output:resize(nframe, nunit)
-      if nunit == 1 then
-         -- Special case to fix output size of 1 bug:
-         self.output:zero():add(self.bias[1])
-         self.output:select(2,1):addmv(1, input, self.weight:select(1,1))
-      else
-         self.output:zero():addr(1, input.new(nframe):fill(1), self.bias)
-         self.output:addmm(1, input, self.weight:t())
-      end
-   else
-      error('input must be vector or matrix')
+function Linear:setup()
+   -- for backwards compatibility
+   if not self.v2 then
+      self._ones = self.weight.new{1}
+      self.v2 = true
    end
+end
 
-   return self.output
+function Linear:updateOutput(input)
+   self:setup()
+   return input.nn.Linear_updateOutput(self, input)
 end
 
 function Linear:updateGradInput(input, gradOutput)
    if self.gradInput then
-
-      local nElement = self.gradInput:nElement()
-      self.gradInput:resizeAs(input)
-      if self.gradInput:nElement() ~= nElement then
-         self.gradInput:zero()
-      end
-      if input:dim() == 1 then
-         self.gradInput:addmv(0, 1, self.weight:t(), gradOutput)
-      elseif input:dim() == 2 then
-         self.gradInput:addmm(0, 1, gradOutput, self.weight)
-      end
-
-      return self.gradInput
+      self:setup()
+      return input.nn.Linear_updateGradInput(self, input, gradOutput)
    end
 end
 
 function Linear:accGradParameters(input, gradOutput, scale)
-   scale = scale or 1
-
-   if input:dim() == 1 then
-      self.gradWeight:addr(scale, gradOutput, input)
-      self.gradBias:add(scale, gradOutput)      
-   elseif input:dim() == 2 then
-      local nframe = input:size(1)
-      local nunit = self.bias:size(1)
-      
-      if nunit == 1 then
-         -- Special case to fix output size of 1 bug:
-         self.gradWeight:select(1,1):addmv(scale, input:t(), gradOutput:select(2,1))
-         self.gradBias:addmv(scale, gradOutput:t(), input.new(nframe):fill(1))
-      else
-         self.gradWeight:addmm(scale, gradOutput:t(), input)
-         self.gradBias:addmv(scale, gradOutput:t(), input.new(nframe):fill(1))
-      end
-   end
-
+   self:setup()
+   return input.nn.Linear_accGradParameters(self, input, gradOutput, scale)
 end
 
 -- we do not need to accumulate parameters when sharing
