87ad85bc6ff3e1505ccbd505c6ccb0c0f1b9a112
Nicholas Leonard <nick@nikopia.org>
2015-02-06 13:22:01 -0500

    Module:listModules()

--- Module.lua
+++ Module.lua
@@ -287,3 +287,27 @@ function Module:findModules(typename, container)
   return nodes, containers
 end
 
+-- returns a list of modules
+function Module:listModules()
+   local function tinsert(to, from)
+      if torch.type(from) == 'table' then
+         for i=1,#from do
+            tinsert(to,from[i])
+         end
+      else
+         table.insert(to,from)
+      end
+   end
+   -- include self first
+   local modules = {self}
+   if self.modules then
+      for i=1,#self.modules do
+         local modulas = self.modules[i]:listModules()
+         if modulas then
+            tinsert(modules,modulas)
+         end
+      end
+   end
+   return modules
+end
+
