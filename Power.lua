24e666bbb8c41484df98ce44e89b56c2780a7ca5
Michael Rotman <rotmanmi@pc-wolf111.(none)>
2015-03-05 11:37:42 +0200

    Now you can use power on inputs with 0s. (there is no longer division by 0)

--- Power.lua
+++ Power.lua
@@ -1,21 +1,21 @@
 local Power, parent = torch.class('nn.Power','nn.Module')
 
 function Power:__init(p)
-   parent.__init(self)
-   self.pow = p
-   if not p then
-      error('nn.Power(power)')
-   end
+	parent.__init(self)
+	self.pow = p
+	if not p then
+		error('nn.goodPower(power)')
+	end
 end
 
 function Power:updateOutput(input)
-   self.output:resizeAs(input):copy(input)
-   self.output:pow(self.pow)
-   return self.output
+	self.output:resizeAs(input):copy(input)
+	self.output:pow(self.pow)
+	return self.output
 end
 
 function Power:updateGradInput(input, gradOutput)
-   self.gradInput:resizeAs(input):copy(gradOutput)
-   self.gradInput:cmul(self.output):cdiv(input):mul(self.pow)
-   return self.gradInput
+	self.gradInput:resizeAs(input):copy(gradOutput)
+	self.gradInput:cmul(torch.pow(input,self.pow-1)):mul(self.pow)
+	return self.gradInput
 end
