149e0bdd84d12d9cb59e39eeb99b4c0102764d19
Edward Grefenstette <etg@google.com>
2015-03-18 13:12:47 +0000

    nn.Replicate can now replicate across other dimensions than the first

--- Replicate.lua
+++ Replicate.lua
@@ -1,20 +1,34 @@
 local Replicate, parent = torch.class('nn.Replicate','nn.Module')
 
-function Replicate:__init(nf)
+function Replicate:__init(nf, dim)
    parent.__init(self)
    self.nfeatures = nf
+   assert(dim > 0, "Can only replicate across positive integer dimensions.")
+   self.dim = dim or 1
 end
 
 function Replicate:updateOutput(input)
+   assert(
+      self.dim <= input:dim()+1,
+      "Not enough input dimensions to replicate along dimension " ..
+      tostring(self.dim) .. ".")
    local sz = torch.LongStorage(input:dim()+1)
-   sz[1] = self.nfeatures
+   sz[self.dim] = self.nfeatures
    for i = 1,input:dim() do
-      sz[i+1] = input:size(i)
+      local offset = 0
+      if i >= self.dim then
+         offset = 1
+      end
+      sz[i+offset] = input:size(i)
    end
    local st = torch.LongStorage(input:dim()+1)
-   st[1] = 0
+   st[self.dim] = 0
    for i = 1,input:dim() do
-      st[i+1] = input:stride(i)
+      local offset = 0
+      if i >= self.dim then
+         offset = 1
+      end
+      st[i+offset] = input:stride(i)
    end
    self.output = input.new(input:storage(),input:storageOffset(),sz,st)
    return self.output
@@ -22,8 +36,8 @@ end
 
 function Replicate:updateGradInput(input, gradOutput)
    self.gradInput:resizeAs(input):zero()
-   for k = 1,gradOutput:size(1) do
-      self.gradInput:add(gradOutput[k])
+   for k = 1,gradOutput:size(self.dim) do
+      self.gradInput:add(gradOutput:select(self.dim, k))
    end
    return self.gradInput
 end
