5bebc3b2f8ec1bf7400206aff5af65b81e28931b
karpathy <andrej.karpathy@gmail.com>
2015-05-05 20:40:24 -0700

    Implemented heavy caching, thank you @soumith for suggestions

--- L2Normalize.lua
+++ L2Normalize.lua
@@ -11,8 +11,11 @@ function L2Normalize:updateOutput(input)
    assert(input:dim() == 2, 'only mini-batch supported (2D tensor), got '
              .. input:dim() .. 'D tensor instead')
    self.output:resizeAs(input)
-   local norms = torch.cmul(input,input):sum(2):sqrt()
-   self.output:copy(input):cdiv(norms:expandAs(input))
+   self.buffer = self.buffer or input.new()
+   self.normSquared = self.normSquared or input.new()
+   self.normSquared:sum(self.buffer:cmul(input, input), 2)
+   self.buffer:sqrt(self.normSquared)
+   self.output:copy(input):cdiv(self.buffer:expandAs(input))
    return self.output
 end
 
@@ -21,20 +24,18 @@ function L2Normalize:updateGradInput(input, gradOutput)
    assert(gradOutput:dim() == 2, 'only mini-batch supported')
    local n = input:size(1) -- batch size
    local d = input:size(2) -- dimensionality of vectors
-
-   local sums = torch.sum(torch.cmul(input,input), 2):view(-1)
-   local divterms = torch.pow(sums,3/2)
    -- compute diagonal term
-   local diag = torch.eye(d):typeAs(input):repeatTensor(n,1):view(n,d,d)
-                   :cmul(sums:view(n,1,1):expand(n,d,d))
+   self.eye = self.eye or torch.eye(d):typeAs(input):repeatTensor(n,1):view(n,d,d)
+   self.diag = self.diag or self.eye.new()
+   self.diag:cmul(self.eye, self.normSquared:view(n,1,1):expand(n,d,d))
    -- compute cross term
-   local b1 = input:reshape(n,d,1)
-   local b2 = input:reshape(n,1,d)
-   local cross = - torch.bmm(b1,b2)
+   local b1 = input:view(n,d,1)
+   local b2 = input:view(n,1,d)
+   self.diag:add(-torch.bmm(b1,b2))
    -- compute the local gradient of the L2 transformation
-   local dsum = torch.cdiv(diag + cross, divterms:view(n,1,1):expand(n,d,d))
+   self.buffer:pow(3)
+   self.diag:cdiv(self.buffer:view(n,1,1):expand(n,d,d))
    -- chain the gradient
-   self.gradInput = torch.bmm(dsum, gradOutput:view(n,d,1)):squeeze()
+   self.gradInput:resize(n,d,1):bmm(self.diag, gradOutput:view(n,d,1)):resize(n,d)
    return self.gradInput
 end
-
