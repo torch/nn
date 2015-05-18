1e7eec6f5c91c40b6dfa7509ae928bf3a54ba0d2
nicholas-leonard <nick@nikopia.org>
2015-05-18 12:44:28 -0400

    MixtureTable lazy initialized buffers

--- utils.lua
+++ utils.lua
@@ -14,4 +14,21 @@ function nn.utils.recursiveType(param, type_str)
    return param
 end
 
+function nn.utils.recursiveResizeAs(t1,t2)
+   if torch.type(t2) == 'table' then
+      t1 = (torch.type(t1) == 'table') and t1 or {t1}
+      for key,_ in pairs(t2) do
+         t1[key], t2[key] = nn.utils.recursiveResizeAs(t1[key], t2[key])
+      end
+   elseif torch.isTensor(t2) then
+      t1 = torch.isTensor(t1) and t1 or t2.new()
+      t1:resizeAs(t2)
+   else
+      error("expecting nested tensors or tables. Got "..
+            torch.type(t1).." and "..torch.type(t2).." instead")
+   end
+   return t1, t2
+end
+
+
 table.unpack = table.unpack or unpack
