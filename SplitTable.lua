01ead4b4d023100ed352cf540e7d9d0fa9a8d852
rfaulk <rfaulk@google.com>
2015-06-17 18:37:27 +0100

    class docs for this module

--- SplitTable.lua
+++ SplitTable.lua
@@ -1,3 +1,32 @@
+--[[ An nn.Module that splits a tensor into evenly sized sub-tensors
+numerically indexed in a lua table.  The sub-tensors will be split along
+the dimension corresponding to the first argument of the constructor.
+
+This model is the inverse of JoinTable.
+
+Example:
+
+> require 'nn'
+> require 'torch'
+> st = nn.SplitTable(1, 2)
+> t = torch.rand(2,2)
+> =t
+ 0.7110  0.1456
+ 0.2049  0.1312
+  [torch.DoubleTensor of size 2x2]
+
+> require 'pprint'
+> pprint(st:updateOutput(t))
+{ 0.7110
+  0.1456
+ [torch.DoubleTensor of size 2],
+  0.2049
+  0.1312
+  [torch.DoubleTensor of size 2]
+}
+
+]]
+
 local SplitTable, parent = torch.class('nn.SplitTable', 'nn.Module')
 
 function SplitTable:__init(dimension, nInputDims)
