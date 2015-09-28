f4da9113874eb42ae8e2b1b997bf10b836d80974
Michael Rotman <rotmanmi@pc-wolf102.(none)>
2015-09-28 12:03:05 +0300

    Added LeakyReLU
    Checking that the multiplication value is positive

---
+++ LeakyReLU.lua
@@ -0,0 +1,28 @@
+local LeakyReLU, parent = torch.class('nn.LeakyReLU','nn.Module')
+
+function LeakyReLU:__init(negval,ip)
+   parent.__init(self)
+   self.negval = negval or (1/100)
+   if  (negval and type(negval) ~= 'number') then
+      error('nn.LeakyReLU(value)')
+   end
+   -- default for inplace is false
+   self.inplace = ip or false
+   if negval < 0 then
+      self.inplace = false
+   end
+   if (ip and type(ip) ~= 'boolean') then
+      error('in-place flag must be boolean')
+   end
+end
+
+function LeakyReLU:updateOutput(input)
+   input.nn.LeakyReLU_updateOutput(self, input)
+   return self.output
+end
+
+function LeakyReLU:updateGradInput(input, gradOutput)
+   input.nn.LeakyReLU_updateGradInput(self, input, gradOutput)
+   return self.gradInput
+end
+
