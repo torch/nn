b1ec1c1ddade74f06fe610ed589f9cfefdf22763
Luke Hewitt <insperatum@gmail.com>
2015-11-15 20:02:45 -0500

    State variable pointers should never change.
    see: https://github.com/torch/nn/blob/master/doc/module.md#state-variables

--- Identity.lua
+++ Identity.lua
@@ -1,12 +1,12 @@
 local Identity, _ = torch.class('nn.Identity', 'nn.Module')
 
 function Identity:updateOutput(input)
-   self.output = input
+   self.output:set(input:storage(), input:storageOffset(), input:size(), input:stride())
    return self.output
 end
 
 
 function Identity:updateGradInput(input, gradOutput)
-   self.gradInput = gradOutput
+   self.gradInput:set(gradOutput:storage(), gradOutput:storageOffset(), gradOutput:size(), gradOutput:stride())
    return self.gradInput
 end
