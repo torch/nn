68f61cf984f582ed3d4ece5d9e9073f19e57345e
Andreas Köpf <andreas.koepf@xamla.com>
2016-01-25 23:23:02 +0100

    Add THNN conversion of {RReLU, Sigmoid, SmoothL1Criterion,SoftMax, SoftPlus}

--- SoftMax.lua
+++ SoftMax.lua
@@ -1,9 +1,19 @@
 local SoftMax, _ = torch.class('nn.SoftMax', 'nn.Module')
 
 function SoftMax:updateOutput(input)
-   return input.nn.SoftMax_updateOutput(self, input)
+   input.THNN.SoftMax_updateOutput(
+      input:cdata(),
+      self.output:cdata()
+   )
+   return self.output
 end
 
 function SoftMax:updateGradInput(input, gradOutput)
-   return input.nn.SoftMax_updateGradInput(self, input, gradOutput)
+   input.THNN.SoftMax_updateGradInput(
+      input:cdata(),
+      gradOutput:cdata(),
+      self.gradInput:cdata(),
+      self.output:cdata()
+   )
+   return self.gradInput
 end
