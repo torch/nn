720ef4236302ffee1cb396d0317d930456ee7cd3
Benni <ben.athiwaratkun@gmail.com>
2016-02-05 16:44:50 -0500

    Add gradient backprop for dropout in evaluation mode

--- Dropout.lua
+++ Dropout.lua
@@ -45,7 +45,14 @@ function Dropout:updateGradInput(input, gradOutput)
          self.gradInput:cmul(self.noise) -- simply mask the gradients with the noise vector
       end
    else
-      error('backprop only defined while training')
+      if self.inplace then
+         self.gradInput = gradOutput
+      else
+         self.gradInput:resizeAs(gradOutput):copy(gradOutput)
+      end
+      if not self.v2 and self.p > 0 then
+         self.gradInput:cdiv(1-self.p)
+      end
    end
    return self.gradInput
 end
