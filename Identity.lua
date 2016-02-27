9b1d49d4ac6bc82971a89b83b92da7bb0c3c3539
Francisco Massa <fvsmassa@gmail.com>
2016-02-27 16:26:27 +0100

    Identity recursively uses :set instead of assignment

--- Identity.lua
+++ Identity.lua
@@ -1,30 +1,47 @@
 local Identity, _ = torch.class('nn.Identity', 'nn.Module')
 
+local function isSameType(a, b)
+   return torch.type(a) == torch.type(b)
+end
+
+local function identity(out, input)
+   if torch.isTensor(input) then
+      -- out could have been a table in a previous call
+      if not isSameType(out, input) then
+         out = input.new()
+      end
+      out:set(input)
+   elseif torch.type(input) ~= 'table' then
+      -- copy numbers, strings or functions
+      out = input
+   else
+      -- avoid recreating tables and tensors
+      -- every time the function is called
+      if not isSameType(out, input) then
+         out = {}
+      end
+      -- recursively sets the entries of out
+      for k,v in pairs(input) do
+         out[k] = identity(out[k], v)
+      end
+      -- clean up behind if the current input is
+      -- smaller than the previous one
+      for k in pairs(out) do
+         if not input[k] then
+            out[k] = nil
+         end
+      end
+   end
+   return out
+end
+
 function Identity:updateOutput(input)
-   self.output = input
+   self.output = identity(self.output, input)
    return self.output
 end
 
 
 function Identity:updateGradInput(input, gradOutput)
-   self.gradInput = gradOutput
+   self.gradInput = identity(self.gradInput, gradOutput)
    return self.gradInput
-end
-
-function Identity:clearState()
-   -- don't call set because it might reset referenced tensors
-   local function clear(f)
-      if self[f] then
-         if torch.isTensor(self[f]) then
-            self[f] = self[f].new()
-         elseif type(self[f]) == 'table' then
-            self[f] = {}
-         else
-            self[f] = nil
-         end
-      end
-   end
-   clear('output')
-   clear('gradInput')
-   return self
 end
