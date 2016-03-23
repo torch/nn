3db9327761553d6bd2ce571615ee70f747781931
rfaulk <rfaulk@google.com>
2016-03-23 14:25:47 +0000

    try to avoid recreating new Tensors. Seems not to be an option with maskedSelect.

--- MaskedSelect.lua
+++ MaskedSelect.lua
@@ -2,26 +2,28 @@ local MaskedSelect, parent = torch.class('nnd.MaskedSelect', 'nn.Module')
 
 --[[ Sets the provided mask value for the module. ]]
 function MaskedSelect:__init()
-  -- mask must be a tensor, convert to byte tensor
   parent.__init(self)
+  self._maskIndices = torch.Tensor()
+  self._buffer = torch.Tensor()
+  self._gradMask = torch.Tensor()
 end
 
 --[[ Performs maskedSelect operation. ]]
 function MaskedSelect:updateOutput(input)
   local input, mask = unpack(input)
-  self.output:maskedSelect(input, mask)
-  self._maskIndices =
-    torch.range(1, mask:nElement()):resize(mask:size()):maskedSelect(mask)
+  self._maskIndices:range(1, mask:nElement()):resize(mask:size())
+  self._maskIndices = self._maskIndices:maskedSelect(mask):long()
+  self.output = input:maskedSelect(mask)
   return self.output
 end
 
 --[[ Reverse maps unmasked gradOutput back to gradInput. ]]
 function MaskedSelect:updateGradInput(input, gradOutput)
   local input, mask = unpack(input)
-  local gradInput = torch.Tensor(input:nElement()):zero()
-  gradInput:scatter(1, self._maskIndices:long(), gradOutput)
-  gradInput:resize(input:size())
-  self.gradInput = {gradInput, torch.Tensor():resize(mask:size()):fill(0)}
+  self._buffer:resize(input:nElement()):zero()
+  self._buffer:scatter(1, self._maskIndices, gradOutput)
+  self._buffer:resize(input:size())
+  self.gradInput = {self._buffer, self._gradMask:resize(mask:size()):fill(0)}
   return self.gradInput
 end
 
@@ -34,17 +36,28 @@ end
 
 function MaskedSelect:type(type)
   local maskIndices = self._maskIndices
+  local gradMask = self._gradMask
+  local buffer = self._buffer
   if type ~= 'torch.CudaTensor' then
-    -- ByteTensors must remain ByteTensors
     self._maskIndices = nil
+    self._gradMask = nil
+    self._gradMask = nil
   end
+
   parent.type(self, type)
+
   if type ~= 'torch.CudaTensor' then
-    self._maskIndices = maskIndices:byte()
+    self._maskIndices = maskIndices:long()
+    self._buffer = buffer:double()
+    self._gradMask = gradMask:byte()
   end
   return self
 end
 
 function MaskedSelect:clearStates()
-  nn.utils.clear(self, {'output', 'gradInput', '_maskIndices'})
+  nn.utils.clear(self, {'output',
+                        'gradInput',
+                        '_maskIndices',
+                        '_buffer',
+                        '_gradMask'})
 end
