abc3930182b5ea0731e41495bd117e89af6ac5dc
kmul00 <coolkoustav@gmail.com>
2016-04-10 17:38:49 +0530

    Added Softened version of SoftMax new file:   SoftenedSoftMax.lua modified:   init.lua modified:   test.lua

--- test.lua
+++ test.lua
@@ -1728,6 +1728,21 @@ function nntest.Softmax()
    mytester:asserteq(berr, 0, torch.typename(module) .. ' - i/o backward err ')
 end
 
+function nntest.SoftenedSoftMax()
+   local ini = math.random(3,5)
+   local ink = math.random(3,5)
+   local input = torch.Tensor(ink, ini):zero()
+   local temp = math.random(1, 5)
+   local module = nn.SoftenedSoftMax(temp)
+   
+   local err = jac.testJacobian(module,input)
+   mytester:assertlt(err,expprecision, 'error on state ')
+
+   local ferr,berr = jac.testIO(module,input)
+   mytester:asserteq(ferr, 0, torch.typename(module) .. ' - i/o forward err ')
+   mytester:asserteq(berr, 0, torch.typename(module) .. ' - i/o backward err ')
+end
+
 function nntest.SpatialSoftMax()
    local ini = math.random(3,5)
    local inj = math.random(3,5)
