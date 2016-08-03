1248424112750af551c5289ac422ae84cf066d61
soumith <soumith@fb.com>
2016-08-03 11:17:55 -0700

    adding dummy gradient to nn.Index path

--- Index.lua
+++ Index.lua
@@ -3,7 +3,7 @@ local Index, parent = torch.class('nn.Index', 'nn.Module')
 function Index:__init(dimension)
     parent.__init(self)
     self.dimension = dimension
-    self.gradInput = {self.gradInput}
+    self.gradInput = {self.gradInput, self.gradInput.new()}
 end
 
 function Index:updateOutput(input)
@@ -17,9 +17,9 @@ function Index:updateGradInput(input, gradOutput)
     local t = input[1]
     local index = input[2]
 
+    self.gradInput[2]:resize(index:size()):zero()
     local gradInput = self.gradInput[1] -- no gradient for the index variable
     gradInput:resizeAs(t):zero()
     gradInput:indexAdd(self.dimension, index, gradOutput)
     return self.gradInput
 end
-
