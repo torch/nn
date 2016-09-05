e1f7e812bce5c32a0e7f3703b5c43f224a5caf5b
Arun Mallya - peach <arun.mallya@gmail.com>
2016-09-05 14:20:19 -0500

    Use stable loss and gradient for MultiLabelSoftMarginCriterion

--- MultiLabelSoftMarginCriterion.lua
+++ MultiLabelSoftMarginCriterion.lua
@@ -8,7 +8,7 @@
 -- and with weights:
 -- l(x,y) = - sum_i weights[i] (y[i] * log(p[i]) + (1 - y[i]) * log (1 - p[i]))
 --
---
+-- This uses the stable form of the loss and gradients.
 --]]
 
 
@@ -16,28 +16,46 @@ local MultiLabelSoftMarginCriterion, parent =
 torch.class('nn.MultiLabelSoftMarginCriterion', 'nn.Criterion')
 
 
-function MultiLabelSoftMarginCriterion:__init(weights)
+function MultiLabelSoftMarginCriterion:__init(weights, sizeAverage)
     parent.__init(self)
-    self.lsm = nn.Sigmoid()
-    self.nll = nn.BCECriterion(weights)
+    if sizeAverage ~= nil then
+        self.sizeAverage = sizeAverage
+    else
+        self.sizeAverage = true
+    end
+    self.sigmoid = nn.Sigmoid()
 end
 
 function MultiLabelSoftMarginCriterion:updateOutput(input, target)
-    input = input:nElement() == 1 and input or input:squeeze()
-    target = target:nElement() == 1 and target or target:squeeze()
-    self.lsm:updateOutput(input)
-    self.nll:updateOutput(self.lsm.output, target)
-    self.output = self.nll.output
+    local input_size = input:size()
+    local x = input:view(input:nElement())
+    local t = target:view(target:nElement())
+
+    self.sigmoid:updateOutput(x)
+
+    local indicator = x:ge(0):typeAs(x)
+    self.output = torch.sum(
+        torch.log(1 + torch.exp(x - torch.cmul(x, indicator):mul(2))) 
+        - torch.cmul(x, t - indicator)
+        )
+    
+    if self.sizeAverage then
+        self.output = self.output / input:nElement()
+    end
+    
     return self.output
 end
 
 function MultiLabelSoftMarginCriterion:updateGradInput(input, target)
-    local size = input:size()
-    input = input:nElement() ==1 and input or input:squeeze()
-    target = target:nElement() == 1 and target or target:squeeze()
-    self.nll:updateGradInput(self.lsm.output, target)
-    self.lsm:updateGradInput(input, self.nll.gradInput)
-    self.gradInput:view(self.lsm.gradInput, size)
+    local t = target:view(target:nElement())
+
+    self.gradInput = self.sigmoid.output - t
+    self.gradInput:resizeAs(input)
+
+    if self.sizeAverage then
+        self.gradInput:div(target:nElement())
+    end
+
     return self.gradInput
 end
 
