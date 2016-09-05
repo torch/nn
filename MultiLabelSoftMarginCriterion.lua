db70ce63066b4559724139d4b43385085f896e36
Arun Mallya - peach <arun.mallya@gmail.com>
2016-09-05 16:28:29 -0500

    Add weighting

--- MultiLabelSoftMarginCriterion.lua
+++ MultiLabelSoftMarginCriterion.lua
@@ -23,22 +23,35 @@ function MultiLabelSoftMarginCriterion:__init(weights, sizeAverage)
     else
         self.sizeAverage = true
     end
+    if weights ~= nil then
+        assert(weights:dim() == 1, "weights input should be 1-D Tensor")
+        self.weights = weights
+    end
     self.sigmoid = nn.Sigmoid()
 end
 
 function MultiLabelSoftMarginCriterion:updateOutput(input, target)
     local input_size = input:size()
+    local weights = self.weights
+    if weights ~= nil and target:dim() ~= 1 then
+        weights = self.weights:view(1, target:size(2)):expandAs(target)
+    end
+
     local x = input:view(input:nElement())
     local t = target:view(target:nElement())
-
+    
     self.sigmoid:updateOutput(x)
 
     local indicator = x:ge(0):typeAs(x)
-    self.output = torch.sum(
-        torch.log(1 + torch.exp(x - torch.cmul(x, indicator):mul(2))) 
+    local buffer = torch.log(1 + torch.exp(x - torch.cmul(x, indicator):mul(2))) 
         - torch.cmul(x, t - indicator)
-        )
-    
+
+    if weights ~= nil then
+        buffer:cmul(weights:reshape(weights:nElement()))
+    end
+
+    self.output = torch.sum(buffer)
+
     if self.sizeAverage then
         self.output = self.output / input:nElement()
     end
@@ -47,15 +60,24 @@ function MultiLabelSoftMarginCriterion:updateOutput(input, target)
 end
 
 function MultiLabelSoftMarginCriterion:updateGradInput(input, target)
+    local weights = self.weights
+    if weights ~= nil and target:dim() ~= 1 then
+        weights = self.weights:view(1, target:size(2)):expandAs(target)
+    end
+
     local t = target:view(target:nElement())
 
     self.gradInput = self.sigmoid.output - t
     self.gradInput:resizeAs(input)
 
+    if weights ~= nil then
+        self.gradInput:cmul(weights)
+    end
+
     if self.sizeAverage then
         self.gradInput:div(target:nElement())
     end
-
+    
     return self.gradInput
 end
 
