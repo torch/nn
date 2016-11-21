22ae8e4a46093f2f5c4bb884a6822dbee29bcc72
Sagar M. Waghmare <SW@discoverelement.com>
2016-11-20 22:10:29 -0500

    Working on updateOutput.

--- DistanceRatioCriterion.lua
+++ DistanceRatioCriterion.lua
@@ -1,12 +1,24 @@
 --[[
    Probabilistic Criterion for Triplet Siamese Model for learning embedding.
    Ref: https://arxiv.org/pdf/1412.6622v3.pdf
+
+   loss = -log( exp(-X) / ( exp(-X) + exp(-Y) ) )
+   where
+   X : Distance between similar samples
+   Y : Distance between dissimilar samples
+
+   The loss could be break down to following log expansion
+
+   loss = -log( exp(-X) ) - (-log( exp(-X) + exp(-Y) ))
+   loss = -log( exp(-X) ) + log( exp(-X) + exp(-Y) )
+   loss = -(-X) + log( exp(-X) + exp(-Y) )
+   loss = X + log( exp(-X) + exp(-Y) )
 --]]
 
 local DistanceRatioCriterion, parent = torch.class('nn.DistanceRatioCriterion',
                                                    'nn.Criterion')
 
-function DistanceRatioCriterion:__init()
+function DistanceRatioCriterion:__init(sizeAverage)
    parent.__init(self)
    if sizeAverage ~= nil then
      self.sizeAverage = sizeAverage
@@ -17,12 +29,40 @@ end
 
 -- X : Distance between similar samples
 -- Y : Distance between dissimilar samples
-function DistanceRatioCriterion:updateOutput(X, Y)
-
-   assert(X.nElement() == Y.nElement())
-
-   self.expX = self.expX or X.new()
-   self.expY = self.expY or Y.new()
+function DistanceRatioCriterion:updateOutput(input)
+   assert(#input == 2, "Invalid number of inputs")
    
-end
+   local X = input[1]
+   local Y = input[2]
 
+   local noOfDistances = X:size(1)
+
+   assert(X:nElement() == Y:nElement(), "Number of distances don't match.")
+   assert(X:size(1) == Y:size(1), "Invalid distances' size.")
+
+   -- Compute exp(-X) and exp(-Y)
+   self.expMinusX = self.expMinusX or X.new()
+   self.expMinusY = self.expMinusY or Y.new()
+
+   -- Compute ( exp(-X) + exp(-Y) )
+   self.expMinusX:resizeAs(X):copy(X):mul(-1):exp()
+   self.expMinusY:resizeAs(Y):copy(Y):mul(-1):exp()
+
+   self.sumExpMinusXY = self.sumExpMinusExp or X.new()
+   self.sumExpMinusXY:resizeAs(self.expMinusX):copy(self.expMinusX)
+                     :add(self.expMinusY)
+
+   -- Compute log( exp(-X) + exp(-Y) )
+   self.logSumExpMinusXY = self.logSumExpMinusXY or self.sumExpMinusXY.new()
+   self.logSumExpMinusXY:resizeAs(self.sumExpMinusXY):copy(self.sumExpMinusXY)
+
+   -- Compute log( exp(-X) + exp(-Y) )
+   self.loss = self.loss or self.logSumExpMinusXY.new()
+   self.loss:resizeAs(X):copy(X):add(self.logSumExpMinusXY)
+
+   if self.sizeAverage then
+      return self.loss:sum()/noOfDistances
+   else
+      return self.loss:sum()
+   end
+end
