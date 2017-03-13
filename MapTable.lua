c2f31ea4438145d66bb330cd587be7f8d92ebae6
davidemaz <davidemaz@gmail.com>
2017-03-13 17:03:06 +0100

    LittleFix self.shared was always true

--- MapTable.lua
+++ MapTable.lua
@@ -2,7 +2,7 @@ local MapTable, parent = torch.class('nn.MapTable', 'nn.Container')
 
 function MapTable:__init(module, shared)
    parent.__init(self)
-   self.shared = shared or true
+   if shared ~= nil then self.shared = shared else self.shared = true end
    self.sharedparams = {'weight', 'bias', 'gradWeight', 'gradBias'}
    self.output = {}
    self.gradInput = {}
